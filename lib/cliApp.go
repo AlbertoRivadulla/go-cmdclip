a2d789c73b110d5341bb2c2c913bb6630cf8a046
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2025-10-25 19:53:48 +0200

    Change the interface to use a flex layout

--- lib/cliApp.go
+++ lib/cliApp.go
@@ -3,7 +3,7 @@ package lib
 import (
 	"log"
 
-	"github.com/gdamore/tcell/v2"
+	// "github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -15,11 +15,11 @@ type CliApp struct {
 	StatusCh chan string
 
 	App *tview.Application
-	MainView *tview.Grid
+	MainView *tview.Flex
 
-	CmdSetListTable *tview.Table
-	CmdListTable *tview.Table
-	CmdContentTable *tview.Table
+	CmdSetList *tview.List
+	CmdList *tview.List
+	CmdContentText *tview.TextView
 
 
 	// TODO:
@@ -40,7 +40,7 @@ func (cliApp* CliApp) Initialize(dbDir string) {
 	cliApp.App.SetRoot(cliApp.MainView, true)
 
 	// Focus on the panel with the list of command sets
-	cliApp.App.SetFocus(cliApp.CmdSetListTable)
+	cliApp.App.SetFocus(cliApp.CmdSetList)
 
 	for _, cmdSet := range cliApp.CmdSets {
 		for _, cmd := range cmdSet.Commands {
@@ -51,57 +51,24 @@ func (cliApp* CliApp) Initialize(dbDir string) {
 }
 
 func (cliApp* CliApp) _setupView() {
-	// Initialize the grid layout
-	cliApp.MainView = tview.NewGrid().
-		// SetBorders(true).
-		// SetColumns(40, 40, -1). // Two columns with size 40 chars, the third column occupies the remaining space
-		SetColumns(-1, -1, -2). // Proportional size. The third row will be twice as large as the other two
-		SetRows(0).
-		SetGap(1, 1)
-	
+	// Initialize the different elements in the app
+	cliApp.CmdSetList = tview.NewList().
+		ShowSecondaryText(false)
+	cliApp.CmdSetList.SetBorder(true).
+		SetTitle("Command sets")
+	cliApp.CmdList = tview.NewList().
+		ShowSecondaryText(false)
+	cliApp.CmdList.SetBorder(true).
+		SetTitle("Commands")
+	cliApp.CmdContentText = tview.NewTextView().
+		SetDynamicColors(true)
 
-	// Initialize the different tables in the app
-	cliApp.CmdSetListTable = tview.NewTable()
-	cliApp.CmdListTable = tview.NewTable()
-	cliApp.CmdContentTable = tview.NewTable()
-	// cliApp.CmdContentTable = tview.NewTable().
-	// 	SetBorders(true)
-
-	// Wrap the first two columns in a frame, and add padding and text in their headers
-	cmdSetListFrame := tview.NewFrame(cliApp.CmdSetListTable).
-		SetBorders(0, 0, 0, 0, 1, 1).
-		AddText("[::b]Command sets[::-]", true, tview.AlignCenter, tcell.ColorGreen)
-	cmdSetListFrame.SetBorder(true).
-		SetBorderColor(tcell.ColorWhite)
-	cmdListFrame := tview.NewFrame(cliApp.CmdListTable).
-		SetBorders(0, 0, 0, 0, 1, 1).
-		AddText("[::b]Commands[::-]", true, tview.AlignCenter, tcell.ColorGreen)
-	cmdListFrame.SetBorder(true).
-		SetBorderColor(tcell.ColorWhite)
-
-
-	// // Wrap each in a Frame
-	// commandsFrame := tview.NewFrame(commandsTable).
-	// 	SetBorders(0, 0, 0, 0, 1, 1). // internal padding (top,bottom,left,right,hPad,vPad)
-	// 	AddText("Commands", true, tview.AlignCenter, tcell.ColorGreen)
-	//
-	// detailsFrame := tview.NewFrame(detailsTable).
-	// 	SetBorders(0, 0, 0, 0, 1, 1).
-	// 	AddText("Details", true, tview.AlignCenter, tcell.ColorGreen)
-	//
-
-
-
-	// cliApp.MainView.AddItem(cliApp.CmdSetListTable, 0, 0, 1, 1, 1, 1, true)
-	// cliApp.MainView.AddItem(cliApp.CmdListTable, 0, 1, 1, 1, 1, 1, false)
-	cliApp.MainView.AddItem(cmdSetListFrame, 0, 0, 1, 1, 1, 1, false)
-	cliApp.MainView.AddItem(cmdListFrame, 0, 1, 1, 1, 1, 1, false)
-	cliApp.MainView.AddItem(cliApp.CmdContentTable, 0, 2, 1, 1, 1, 1, false)
-
-
-
-
-
+	// Place the elements in a flex layout
+	cliApp.MainView = tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(cliApp.CmdSetList, 0, 1, true).
+		AddItem(cliApp.CmdList, 0, 1, false).
+		AddItem(cliApp.CmdContentText, 0, 2, false)
 
 
 	// TODO: Populate the column of commands sets
